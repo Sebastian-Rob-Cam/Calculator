d466bc877e14e2ff78c63f09c165f48f815fc5ec
Sebastian Robles Camacho <sebastian.rob.cam@gmail.com>
2024-12-23 18:26:44 -0600

    feat: first attempt for operate inputs.

--- src/main/java/dev/sebastian/calculator/logic/InputHandler.java
+++ src/main/java/dev/sebastian/calculator/logic/InputHandler.java
@@ -17,6 +17,29 @@ public class InputHandler {
 
     public String appendInput(String input) {
         this.input += input;
+        handler();
+
         return this.input;
+    }
+
+    private void handler() {
+        char[] inputArray = input.toCharArray();
+        double result = 0;
+
+        if (inputArray.length > 2) {
+            for (char c : inputArray) {
+                switch (c) {
+                    case '+':
+                        double tempAdd = BasicOperations.add(Double.parseDouble(inputArray[0] + ""),
+                                Double.parseDouble(inputArray[2] + ""));
+
+                        System.out.println("tempAdd: " + tempAdd);
+                    default:
+                        // return result;
+                }
+            }
+        }
+
+        // return result;
     }
 }
