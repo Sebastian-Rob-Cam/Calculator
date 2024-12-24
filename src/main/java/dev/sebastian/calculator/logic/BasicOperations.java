3c2ac16d1b8a83dd27bb8ada9275d3c974b4407f
Sebastian Robles Camacho <sebastian.rob.cam@gmail.com>
2024-12-23 18:27:25 -0600

    feat: create the basic operatios.

---
+++ src/main/java/dev/sebastian/calculator/logic/BasicOperations.java
@@ -0,0 +1,21 @@
+package main.java.dev.sebastian.calculator.logic;
+
+public class BasicOperations {
+
+    public static double add(double a, double b) {
+        System.out.println("running add");
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        return a / b;
+    }
+}
